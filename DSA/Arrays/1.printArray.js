9cf26b23b755da8fa451fe118c5114918fd90397
SheshuArabati <73385927+SheshuArabati@users.noreply.github.com>
2023-07-07 09:35:22 +0530

    Delete DSA/Arrays directory

--- DSA/Arrays/1.printArray.js
+++
@@ -1,4 +0,0 @@
-let arr = [34, 56, 78, 54, 43, 312, 23];
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
--- DSA/Arrays/13.printSubArrayStoE.js
+++
@@ -1,8 +0,0 @@
-// Given arr, print subarray from s to e
-
-let arr = [4, 2, 10, 3, 12, -2, 5];
-let st = 2;
-let en = 4;
-for (let i = st; i <= en; i++) {
-    console.log(arr[i]);
-}
